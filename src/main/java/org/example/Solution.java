9cab14d0e157d06ef79411e4a45fd792d107da30
alek <wait82985@gmail.com>
2023-05-30 17:27:37 +0600

    working recursive top down algo. memoization needed

--- src/main/java/org/example/Solution.java
+++ src/main/java/org/example/Solution.java
@@ -1,8 +1,21 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Solution {
     public int coinChange(int[] coins, int amount) {
-        //TODO
-        return 0;
+        Arrays.sort(coins);
+
+      int result = dfs(coins, amount, 0);
+      return result == Integer.MAX_VALUE ? -1 : result;
+    }
+    private int dfs(int[] coins, int amount, int count){
+        if(amount < coins[0] && amount < 0) return Integer.MAX_VALUE;
+        if(amount == 0) return count;
+        int result = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+           result = Math.min(result, dfs(coins, amount - coins[i], count + 1));
+        }
+        return result;
     }
 }
