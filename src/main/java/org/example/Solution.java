7cf5644ab1d6177852081ecbbcb34411c87f43de
alek <wait82985@gmail.com>
2023-05-30 16:19:31 +0600

    initial commit. 3 tats cases

---
+++ src/main/java/org/example/Solution.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Solution {
+    public int coinChange(int[] coins, int amount) {
+        //TODO
+        return 0;
+    }
+}
---
+++ src/test/java/SolutionTest.java
@@ -0,0 +1,29 @@
+import org.example.Solution;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void coinChangeTest1(){
+        int[] coins = {1,2,5};
+        int amount = 11;
+        int result = 3;
+        Assert.assertEquals(result, new Solution().coinChange(coins, amount));
+    }
+
+    @Test
+    public void coinChangeTest2(){
+        int[] coins = {2};
+        int amount = 3;
+        int result = -1;
+        Assert.assertEquals(result, new Solution().coinChange(coins, amount));
+    }
+
+    @Test
+    public void coinChangeTest3(){
+        int[] coins = {1};
+        int amount = 0;
+        int result = 0;
+        Assert.assertEquals(result, new Solution().coinChange(coins, amount));
+    }
+}
